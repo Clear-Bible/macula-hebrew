6ae4f6bb8089b637fbad46eadf2f1d77d8140071
Mike Brinker <themikejr@gmail.com>
2022-10-10 11:26:47 -0400

    Add more lowfat tests.

--- test/test_lowfat.py
+++ test/test_lowfat.py
@@ -23,6 +23,30 @@ def test_file_is_valid_xml(lowfat_file):
     assert etree.parse(lowfat_file)
 
 
+@pytest.mark.parametrize("lowfat_file", __lowfat_files__)
+def test_ref_attr_correct_format(lowfat_file):
+    pattern = "^[A-Z0-9]{3} [0-9]+:[0-9]+![0-9]+$"  # USFM Ref
+    nodes = run_xpath_for_file("//w", lowfat_file)
+    for node in nodes:
+        assert node.attrib["ref"] != ""
+        assert re.match(pattern, node.attrib["ref"])
+
+
+@pytest.mark.parametrize("lowfat_file", __lowfat_files__)
+def test_required_attrs_exist_on_w_elements(lowfat_file):
+    required_attrs = [
+        "ref",
+        "class",
+        "{http://www.w3.org/XML/1998/namespace}id",  # @xml:id
+        "morph",
+        "unicode",
+    ]
+    nodes = run_xpath_for_file("//w", lowfat_file)
+    for node in nodes:
+        for attr in required_attrs:
+            assert attr in node.attrib
+
+
 def test_number_of_lowfat_words():
     total_count = 0
     for lowfat_file in __lowfat_files__:
